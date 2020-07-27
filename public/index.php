d9fc7a6101dd577d3fced3812aa43e438685561c
Dmitry Leyko <Dimitry.C.Leyko@informa.com>
2020-07-27 18:41:10 +0300

    Update project files

--- public/index.php
+++ public/index.php
@@ -2,24 +2,24 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use App\Core\Framework;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
 
 function application(): void
 {
     require_once __DIR__ . '/../vendor/autoload.php';
-    $routes = require_once __DIR__ . '/../config/routes.config.php';
-    $request = Request::createFromGlobals();
-    $context = new RequestContext();
-    $matcher = new UrlMatcher($routes, $context);
+    $routes             = require_once __DIR__ . '/../config/routes.config.php';
+    $request            = Request::createFromGlobals();
+    $context            = new RequestContext();
+    $matcher            = new UrlMatcher($routes, $context);
     $controllerResolver = new ControllerResolver();
-    $argumentResolver = new ArgumentResolver();
-    $framework = new Framework($matcher, $controllerResolver, $argumentResolver);
-    $response = $framework->handle($request);
+    $argumentResolver   = new ArgumentResolver();
+    $framework          = new Framework($matcher, $controllerResolver, $argumentResolver);
+    $response           = $framework->handle($request);
     $response->send();
 }
 
